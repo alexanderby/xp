3c87b9eaa268aea35626c0378bb4353f072ab8f7
Alexander Shutov <shutovby@gmail.com>
2014-11-23 21:32:14 +0300

    Bindable object.

---
+++ src/xp/core/binding.ts
@@ -0,0 +1,77 @@
+﻿module xp {
+    /**
+     * Creates bindable object.
+     * Change-notifications can be reached by subscribing to 'onPropertyChanged' event.
+     * Values should be changed through 'data' property object.
+     * @param obj Source object.
+     * @param [propertyNames] If specified, determines which properties' changes should be tracked. In other case all properties' changes would be tracked.
+     */
+    export function createBindableObject<T>(obj: T, propertyNames?: string[]): BindableObject<T> {
+        var b: BindableObject<T> = {
+            onPropertyChanged: new Event<PropertyChangedArgs>(),
+            data: <T>{},
+            source: obj
+        };
+        if (propertyNames) {
+            // Track specified properties
+            propertyNames.forEach((name) => {
+                createProperty(b, name);
+            });
+        }
+        else {
+            // Track all properties
+            for (var key in obj) {
+                createProperty(b, key);
+            }
+        }
+        return b;
+    }
+
+    function createProperty<T>(bObj: BindableObject<T>, name: string) {
+        Object.defineProperty(bObj.data, name, {
+            get: function () {
+                return bObj.source[name];
+            },
+            set: function (value) {
+                var oldValue = bObj.source[name];
+                bObj.source[name] = value;
+                bObj.onPropertyChanged.invoke({
+                    propertyName: name,
+                    oldValue: oldValue,
+                    newValue: value
+                });
+            },
+            enumerable: true,
+            configurable: true
+        });
+    }
+
+    /**
+     * Arguments of object's property changed event.
+     */
+    export interface PropertyChangedArgs {
+        propertyName: string;
+        oldValue: any;
+        newValue: any;
+    }
+
+    /**
+     * Bindable object.
+     * Change-notifications can be reached by subscribing to 'onPropertyChanged' event.
+     * Values should be changed through 'data' property object.
+     */
+    export interface BindableObject<T> {
+        /**
+         * Is invoked when any object's property is changed.
+         */
+        onPropertyChanged: Event<PropertyChangedArgs>;
+        /**
+         * Changes should be made through this object.
+         */
+        data: T;
+        /**
+         * Source object. Changes made to 'data' property will be reflected on this property too.
+         */
+        source: T;
+    }
+} 
