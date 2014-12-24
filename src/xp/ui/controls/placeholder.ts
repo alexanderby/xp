d26df4d81bbc08fb775e158fd3d4ba343d42b97a
Alexander Shutov <shutovby@gmail.com>
2014-12-24 15:04:12 +0300

    Placeholder element.

---
+++ src/xp/ui/controls/placeholder.ts
@@ -0,0 +1,19 @@
+﻿module xp.Ui {
+    /**
+     * Dummy placeholder.
+     */
+    export class Placeholder extends Element {
+
+        protected getTemplate(): JQuery {
+            return $('<span class="placeholder">&nbsp;</span>');
+        }
+
+        setDefaults() {
+            super.setDefaults();
+            this.width = '100%';
+            this.height = '100%';
+        }
+
+    }
+    Tags['placeholder'] = Placeholder;
+}
