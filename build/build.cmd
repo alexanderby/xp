99fe65bf4866230f418c9ac57bd3674d25d82d74
Alexander Shutov <shutovby@gmail.com>
2015-01-27 21:39:31 +0300

    Build script.

---
+++ build/build.cmd
@@ -0,0 +1,9 @@
+PROMPT $g
+SET outfolder=xp
+
+ECHO "*** Compiling TS to single 'xp.js'... ***"
+"C:\Program Files (x86)\Microsoft SDKs\TypeScript\1.1\tsc.exe" @files.txt --out %outfolder%\xp.js -t ES5 --declaration
+
+ECHO "*** Copying contents... ***"
+XCOPY ..\src\xp\schema\markup.xsd %outfolder%\ /d /y
+XCOPY ..\src\xp\style\defaultstyle.css %outfolder%\ /d /y
