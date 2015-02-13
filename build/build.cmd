9f8a68e78791139f16a2be0f1552f5d845fc5c45
Alexander Shutov <shutovby@gmail.com>
2015-02-13 12:59:27 +0300

    Prefix controls' style file with underscore (as not built).

--- build/build.cmd
+++ build/build.cmd
@@ -9,7 +9,7 @@ MD %outfolder%\schema
 COPY ..\src\xp\schema\markup.xsd %outfolder%\schema\ /d /y
 MD %outfolder%\style
 COPY ..\src\xp\style\defaultstyle.css %outfolder%\style\ /d /y
-COPY ..\src\xp\style\xp.css %outfolder%\style\xp.less /d /y
+COPY ..\src\xp\style\xp.css %outfolder%\style\_xp.less /d /y
 COPY ..\src\xp\style\_variables.less %outfolder%\style\ /d /y
 MD %outfolder%\typing
 MOVE /y %outfolder%\xp.d.ts %outfolder%\typing\xp.d.ts
