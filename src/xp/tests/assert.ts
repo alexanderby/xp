373a0613bb0246450ff2dd3d8c564db32ffddaa4
Alexander Shutov <shutovby@gmail.com>
2015-02-21 18:11:16 +0300

    Another method for tests.

--- src/xp/tests/assert.ts
+++ src/xp/tests/assert.ts
@@ -1,6 +1,19 @@
 ﻿module xp.Tests {
 
     /**
+     * Throws an error if statement is not true.
+     */
+    export function assert(statement: boolean) {
+        var t = typeof statement;
+        if (t !== 'boolean')
+            throw new Error('Assert statement must be boolean but not ' + t + '.');
+
+        if (statement !== true) {
+            throw new Error(xp.formatString('Statement is wrong.'));
+        }
+    }
+
+    /**
      * Determines whether two objects are equal.
      * If not throws an error.
      */
