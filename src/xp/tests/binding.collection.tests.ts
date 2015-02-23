ee7725dc7a4bcf807f7ff16f8fcef4601ffeedb2
Alexander Shutov <shutovby@gmail.com>
2015-02-23 18:25:01 +0300

    Cleanup.

--- src/xp/tests/binding.collection.tests.ts
+++ src/xp/tests/binding.collection.tests.ts
@@ -21,70 +21,51 @@
     nc.onPropertyChanged.addHandler((prop) => {
         Log.write(Log.HeatLevel.Log, Log.Domain.Test, 'Test: Collection property changed: "{0}".', prop);
     }, window);
-    Log.write(Log.HeatLevel.Log, Log.Domain.Test, nc);
 
     //
     // Process
 
-    Log.write(Log.HeatLevel.Log, Log.Domain.Test, 'Test: Push.');
     nc.push({ v: 4 }, { v: 5 });
-    Log.write(Log.HeatLevel.Log, Log.Domain.Test, nc);
     assertEqual(
         JSON.stringify(nc),
         JSON.stringify([{ v: 1 }, { v: 2 }, { v: 3 }, { v: 4 }, { v: 5 }]));
 
-    Log.write(Log.HeatLevel.Log, Log.Domain.Test, 'Test: Pop.');
     nc.pop();
-    Log.write(Log.HeatLevel.Log, Log.Domain.Test, nc);
     assertEqual(
         JSON.stringify(nc),
         JSON.stringify([{ v: 1 }, { v: 2 }, { v: 3 }, { v: 4 }]));
 
-    Log.write(Log.HeatLevel.Log, Log.Domain.Test, 'Test: Shift.');
     nc.shift();
-    Log.write(Log.HeatLevel.Log, Log.Domain.Test, nc);
     assertEqual(
         JSON.stringify(nc),
         JSON.stringify([{ v: 2 }, { v: 3 }, { v: 4 }]));
 
-    Log.write(Log.HeatLevel.Log, Log.Domain.Test, 'Test: Unshift.');
     nc.unshift({ v: 0 });
-    Log.write(Log.HeatLevel.Log, Log.Domain.Test, nc);
     assertEqual(
         JSON.stringify(nc),
         JSON.stringify([{ v: 0 }, { v: 2 }, { v: 3 }, { v: 4 }]));
 
-    Log.write(Log.HeatLevel.Log, Log.Domain.Test, 'Test: Splice.');
     nc.splice(1, 2, { v: 7 }, { v: 8 });
-    Log.write(Log.HeatLevel.Log, Log.Domain.Test, nc);
     assertEqual(
         JSON.stringify(nc),
         JSON.stringify([{ v: 0 }, { v: 7 }, { v: 8 }, { v: 4 }]));
 
-    Log.write(Log.HeatLevel.Log, Log.Domain.Test, 'Test: Replace.');
     nc[2] = { v: 9 };
-    Log.write(Log.HeatLevel.Log, Log.Domain.Test, nc);
     assertEqual(
         JSON.stringify(nc),
         JSON.stringify([{ v: 0 }, { v: 7 }, { v: 9 }, { v: 4 }]));
 
-    Log.write(Log.HeatLevel.Log, Log.Domain.Test, 'Test: Move.');
     nc.move(1, 3);
-    Log.write(Log.HeatLevel.Log, Log.Domain.Test, nc);
     assertEqual(
         JSON.stringify(nc),
         JSON.stringify([{ v: 0 }, { v: 9 }, { v: 4 }, { v: 7 }]));
 
-    Log.write(Log.HeatLevel.Log, Log.Domain.Test, 'Test: Sort.');
     nc.sort((a, b) => a.v - b.v);
-    Log.write(Log.HeatLevel.Log, Log.Domain.Test, nc);
     assertEqual(
         JSON.stringify(nc),
         JSON.stringify([{ v: 0 }, { v: 4 }, { v: 7 }, { v: 9 }]));
 
-    Log.write(Log.HeatLevel.Log, Log.Domain.Test, 'Test: Reverse.');
     nc.reverse();
-    Log.write(Log.HeatLevel.Log, Log.Domain.Test, nc);
     assertEqual(
         JSON.stringify(nc),
         JSON.stringify([{ v: 9 }, { v: 7 }, { v: 4 }, { v: 0 }]));
--- src/xp/tests/binding.manager.tests.ts
+++ src/xp/tests/binding.manager.tests.ts
@@ -25,24 +25,18 @@
     };
 
     // Create manager
-    Log.write(Log.HeatLevel.Log, Log.Domain.Test, 'Test: Create binding manager.');
     var manager = new xp.BindingManager(target, 'cityName', new Scope(source), 'city.name', 'unknown');
     assertEqual(source.city.name, target.cityName);
     assertEqual(source['onPropertyChanged']['handlers'].length, 1);
     assertEqual(source.city['onPropertyChanged']['handlers'].length, 1);
 
     // Change name
-    Log.write(Log.HeatLevel.Log, Log.Domain.Test, 'Test: Change name.');
     source.name = 'Ivan';
-    Log.write(Log.HeatLevel.Log, Log.Domain.Test, source.name, 'Ivan');
 
     // Change city name
-    Log.write(Log.HeatLevel.Log, Log.Domain.Test, 'Test: Change city name.');
     source.city.name = 'Moscow';
-    Log.write(Log.HeatLevel.Log, Log.Domain.Test, source.city.name, 'Moscow');
 
     // Change city
-    Log.write(Log.HeatLevel.Log, Log.Domain.Test, 'Test: Change city.');
     var oldCity = source.city;
     source.city = <any>{ name: 'Hoiniki' };
     assertEqual(source.city.name, 'Hoiniki');
@@ -51,12 +45,10 @@
     assertEqual(oldCity['onPropertyChanged']['handlers'].length, 0);
 
     // Change city to null
-    Log.write(Log.HeatLevel.Log, Log.Domain.Test, 'Test: Change city to NULL.');
     source.city = null;
     assertEqual(target.cityName, 'unknown');
 
     // Revert city
-    Log.write(Log.HeatLevel.Log, Log.Domain.Test, 'Test: Revert city.');
     source.city = oldCity;
     assertEqual(source.city.name, 'Moscow');
     assertEqual(target.cityName, 'Moscow');
@@ -64,12 +56,10 @@
     assertEqual(source.city['onPropertyChanged']['handlers'].length, 1);
 
     // Change source to null
-    Log.write(Log.HeatLevel.Log, Log.Domain.Test, 'Test: Change source to NULL.');
     manager.resetWith(null);
     assertEqual(target.cityName, 'unknown');
 
     // Revert source
-    Log.write(Log.HeatLevel.Log, Log.Domain.Test, 'Test: Revert source.');
     manager.resetWith(new Scope(source));
     assertEqual(source.city.name, 'Moscow');
     assertEqual(target.cityName, 'Moscow');
